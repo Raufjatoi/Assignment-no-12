bad76683edb609c97cdd72bb476051859b222770
Abdul rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-02-26 21:27:11 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,21 +1,42 @@
 # 3. Shopping List Manager:
 #  Develop a program that allows users to add, remove, and check off items on a shopping list 
 # stored in a dictionary.
-items = {'apple': 10, 'banana': 14, 'orange': 8}
-print(items)
+dic = {}
+while True :
+ def add (i,v):
+    if i not in dic :
+        dic[i] = int(v)        
+ def remove (i):
+    if i in dic :
+        del dic[i] 
+ def check (i,g):
+    if i in dic:
+        dic[i] -= int(g)
+ def view ():
+    for key,value in dic.items():
+        print(f'{key}: {value}')
+ print('Enter choice : \n 1 : Add  \n 2 : remove \n 3 : check \n 4 : view ')
+ c = int(input("enter choice : "))
+ if c == 1 :
+    i = input('Enter item : ')
+    v = input('Enter val : ')
+    add(i,v)
+ elif c == 2 :
+    i = input('Enter the item u wanna remove : ')
+    remove(i)
+ elif c == 3 :
+    i = input('Enter wha ya wanna remove  : ')
+    v = int(input('How many : '))
+    check(i,v)
+ elif c == 4 : 
+    view()
+ elif i == 0:
+    print('Thank u for using :::: ')
+    break
+ else:
+    print('Invalid ')
+     
 
-user = input('Enter "a" to add and "r" to remove: ').lower()
 
-if user == 'r':
-    r = input('Which item do you want to remove: ').lower()
-    if r in items:
-        del items[r]
-        print(f'{r.capitalize()} removed. Updated items: {items}')
-    else:
-        print(f'{r} not found.')
+
     
-elif user == 'a':
-    a = input('What do you want to add: ')
-    q = int(input('Enter quantity: '))
-    items[a] = q
-    print(f'{q} {a.capitalize()}(s) added. Updated items: {items}')
